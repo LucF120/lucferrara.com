2693e5f38cd012f9976367624f7bbea87067ad3d
Luc Ferrara <lucjferrara@gmail.com>
2025-04-01 12:49:17 -0400

    Created coming soon page

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Coming Soon!</h1>
+      <p>Luc Ferrara's site is coming soon. Stay tuned!</p>
+    </div>
   )
 }
-
 export default App
