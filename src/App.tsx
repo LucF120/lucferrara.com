acd740ee38f346c7a78857f63e0a4e76966fe679
Luc Ferrara <lucjferrara@gmail.com>
2025-04-01 12:57:25 -0400

    Trying to make doc title change

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    document.title = "Coming Soon 🚀"; // Set tab title when component mounts
+    return () => {
+      document.title = "Default Title"; // Reset if needed when unmounted
+    };
+  }, []);
+  
   return (
     <div>
       <h1>Coming Soon!</h1>
