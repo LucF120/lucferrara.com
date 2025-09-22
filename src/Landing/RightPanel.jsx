886c1b997bb24fe892f443bee0fda58ccad8db27
LucF120 <lucjferrara@gmail.com>
2025-09-21 21:09:39 -0400

    centering image fix

--- src/Landing/RightPanel.jsx
+++ src/Landing/RightPanel.jsx
@@ -5,7 +5,7 @@ import Projects from "./Projects";
 export default function RightPanel () {
     return (
         <div className="d-flex flex-column align-items-center">
-              <div>
+              <div className="d-flex justify-content-center">
                 <img src="me.jpg" 
                     style={{
                       width: "min(200px, 100%)",
